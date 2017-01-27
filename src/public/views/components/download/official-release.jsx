05d880830b91a932d70857b2426d6f4202545058
Andrew Stone <g.andrew.stone@gmail.com>
2017-01-26 23:08:38 -0500

    1.0.0 releases

--- src/public/views/components/download/official-release.jsx
+++ src/public/views/components/download/official-release.jsx
@@ -10,7 +10,7 @@ export default React.createClass({
             <div className='row service-wrapper-row'>
                 <div className='col-sm-4'>
                     <div className='service-image'>
-                        <img src='/img/fingerprint.svg' alt='Fingerprint' />
+                        <img src='/img/download.svg' alt='download' />
                     </div>
                 </div>
                 <div className='col-sm-8'>
@@ -29,21 +29,25 @@ export default React.createClass({
                     <h3>{strings().official.binary}</h3>
                     <br />
                     <h4>{strings().official.windows}</h4>
-                    <p>{strings().official.bit64}: {strings().official.version}: 0.12.1, {strings().official.august14}: <a href='/downloads/bitcoinUnlimited-0.12.1-win64-setup.exe'> bitcoinUnlimited-0.12.1-win64-setup.exe</a> <a href='/downloads/bitcoinUnlimited-0.12.1-win64.zip'> (zip)</a></p>
-                    <p>{strings().official.bit32}: {strings().official.version}: 0.12.1, {strings().official.august14}: <a href='/downloads/bitcoinUnlimited-0.12.1-win32-setup.exe'> bitcoinUnlimited-0.12.1-win32-setup.exe</a> <a href='/downloads/bitcoinUnlimited-0.12.1-win32.zip'> (zip)</a></p>
+                    <p>{strings().official.bit64}: {strings().official.version}: 1.0.0, {strings().official.date}: <a href='/downloads/bitcoinUnlimited-1.0.0-win64-setup.exe'> bitcoinUnlimited-1.0.0-win64-setup.exe</a> <a href='/downloads/bitcoinUnlimited-1.0.0-win64.zip'> (zip)</a></p>
+                    <p>{strings().official.bit32}: {strings().official.version}: 1.0.0, {strings().official.date}: <a href='/downloads/bitcoinUnlimited-1.0.0-win32-setup.exe'> bitcoinUnlimited-1.0.0-win32-setup.exe</a> <a href='/downloads/bitcoinUnlimited-1.0.0-win32.zip'> (zip)</a></p>
                     <br />
                     <h4>{strings().official.linux}</h4>
-                    <p>{strings().official.bit64}: {strings().official.version}: 0.12.1, {strings().official.august14}:<a href='/downloads/bitcoinUnlimited-0.12.1-linux64.tar.gz'> bitcoinUnlimited-0.12.1-linux64.tar.gz</a></p>
-                    <p>{strings().official.bit32}: {strings().official.version}: 0.12.1, {strings().official.august14}: <a href='/downloads/bitcoinUnlimited-0.12.1-linux32.tar.gz'> bitcoinUnlimited-0.12.1-linux32.tar.gz</a></p>
+                    <p>{strings().official.bit64}: {strings().official.version}: 1.0.0, {strings().official.date}:<a href='/downloads/bitcoinUnlimited-1.0.0-linux64.tar.gz'> bitcoinUnlimited-1.0.0-linux64.tar.gz</a></p>
+                    <p>{strings().official.bit32}: {strings().official.version}: 1.0.0, {strings().official.date}: <a href='/downloads/bitcoinUnlimited-1.0.0-linux32.tar.gz'> bitcoinUnlimited-1.0.0-linux32.tar.gz</a></p>
                     <br />
                     <h4>{strings().official.osx}</h4>
-                    <p>{strings().official.bit64}: {strings().official.version}: 0.12.1, {strings().official.august14}:<a href='/downloads/bitcoinUnlimited-0.12.1-osx64.tar.gz'> bitcoinUnlimited-0.12.1-osx64.tar.gz</a></p>
-                    <p>{strings().official.bit32}: {strings().official.version}: 0.12.1, {strings().official.august14}:<a href='/downloads/bitcoinUnlimited-0.12.1-osx.tar.gz'> bitcoinUnlimited-0.12.1-osx.tar.gz</a></p>
-                    <p>DMG: {strings().official.version}: 0.12.1, {strings().official.august14}:<a href='/downloads/bitcoinUnlimited-0.12.1-osx.dmg'> bitcoinUnlimited-0.12.1-osx.dmg</a></p>
+                    <p>{strings().official.bit64}: {strings().official.version}: 1.0.0, {strings().official.date}:<a href='/downloads/bitcoinUnlimited-1.0.0-osx64.tar.gz'> bitcoinUnlimited-1.0.0-osx64.tar.gz</a></p>
+                    <p>{strings().official.bit32}: {strings().official.version}: 1.0.0, {strings().official.date}:<a href='/downloads/bitcoinUnlimited-1.0.0-osx.tar.gz'> bitcoinUnlimited-1.0.0-osx.tar.gz</a></p>
+                    <p>DMG: {strings().official.version}: 1.0.0, {strings().official.date}:<a href='/downloads/bitcoinUnlimited-1.0.0-osx.dmg'> bitcoinUnlimited-1.0.0-osx.dmg</a></p>
+                    <br />
+                    <h4>{strings().official.ARM}</h4>
+                    <p>{strings().official.bit64}: {strings().official.version}: 1.0.0, {strings().official.date}:<a href='/downloads/bitcoinUnlimited-1.0.0-arm64.tar.gz'> bitcoinUnlimited-1.0.0-arm64.tar.gz</a></p>
+                    <p>{strings().official.bit32}: {strings().official.version}: 1.0.0, {strings().official.date}:<a href='/downloads/bitcoinUnlimited-1.0.0-arm32.tar.gz'> bitcoinUnlimited-1.0.0-arm32.tar.gz</a></p>
                     <br />
 
                     <h4>{strings().official.source}</h4>
-                    <p>{strings().official.branch} 0.12.1bu <a href='https://github.com/BitcoinUnlimited/BitcoinUnlimited/tree/0.12.1bu'>{strings().official.here}</a></p>
+                    <p>{strings().official.branch} <a href='https://github.com/BitcoinUnlimited/BitcoinUnlimited/tree/release'>{strings().official.here}</a></p>
                 </div>
             </div>
         );
