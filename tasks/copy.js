7179dc65b0eefbf59e648e6e0df7c6593c796aef
Sina Habibian <sinahab@gmail.com>
2017-03-11 16:40:49 -0800

    Add EmergentConsensus page and /emergent-consensus route

--- tasks/copy.js
+++ tasks/copy.js
@@ -17,6 +17,7 @@ module.exports = function(grunt) {
                         '**/*.ico',
                         '**/*.jpg',
                         '**/*.jpeg',
+                        '**/*.gif',
                         '**/*.svg',
                         '**/*.json',
                         '**/*.pdf',
