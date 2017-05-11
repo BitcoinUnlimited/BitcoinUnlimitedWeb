722b53830a0bc4df3d0f0e22cc690371de7a2900
Andrea Suisani <sickpig@gmail.com>
2017-05-11 12:14:44 +0200

    Remove 2017-05-09 security announcement

--- src/public/views/header.jsx
+++ src/public/views/header.jsx
@@ -7,17 +7,17 @@ import SecurityBanner from './components/header/securityBanner.jsx'
 
 class Header extends React.Component {
     renderSecurityMessage() {
-        //return
-        return (
-             <div className='inline'>
-              <p>
-                 2017-05-09. BU nodes under attack. As temporary countermeasure please disable Xthin.
-              </p>
-              <p>
-                To disable it add <i>use-thinblock=0</i> to your bitcoin.conf or use the command line flag <i>-use-thinblocks=0</i>.
-              </p>
-             </div>
-        )
+        return
+        //return (
+        //     <div className='inline'>
+        //      <p>
+        //         2017-05-09. BU nodes under attack. As temporary countermeasure please disable Xthin.
+        //      </p>
+        //      <p>
+        //        To disable it add <i>use-thinblock=0</i> to your bitcoin.conf or use the command line flag <i>-use-thinblocks=0</i>.
+        //      </p>
+        //     </div>
+        //)
     }
 
     render() {
