ab3c449e556fba0a0e629da9b3d9fc481cc4c487
Pat O'Brien <obrien317@gmail.com>
2018-06-18 13:47:38 -0500

    stashing bitcore tests

--- src/public/views/pages/resources.jsx
+++ src/public/views/pages/resources.jsx
@@ -12,10 +12,48 @@ import Buips from '../components/resources/buips.jsx';
 import NsInfo from '../components/resources/nsInfo.jsx';
 import CashNodes from '../components/resources/cashNodes.jsx';
 
+import Bitcore from 'bitcore-lib'
+import Message from 'bitcore-message'
+
 class Resources extends React.Component {
     constructor(props) {
         super(props);
         this.state = { selectedSection: this.props.params.section };
+
+        var privateKey = Bitcore.PrivateKey.fromWIF('cPBn5A4ikZvBTQ8D7NnvHZYCAxzDZ5Z2TSGW2LkyPiLxqYaJPBW4');
+        var signature = Message('hello, world').sign(privateKey);
+        console.log(signature);
+
+        var address = 'n1ZCYg9YXtB5XCZazLxSmPDa8iwJRZHhGx';
+        var signature2 = 'H/DIn8uA1scAuKLlCx+/9LnAcJtwQQ0PmcPrJUq90aboLv3fH5fFvY+vmbfOSFEtGarznYli6ShPr9RXwY9UrIY=';
+        var verified = Message('hello, world').verify(address, signature);
+        console.log(verified);
+
+        console.log(signature == signature2);
+
+        var verified2 = Message('hello, world').verify(address, signature2);
+        console.log(verified2);
+
+        var addr2 = 'n1ZCYg9YXtB5XCZazLxSmPDa8iwJRZHhGx';
+        // var sig = 'IPynhAQDxJJV6mqWcowsCDXu7i7Xbc5D8M6Ai6UkTg8AfSQQvNS2RiLCIvDJtpVYG/mAV9pRTMjllKa8ZI43/64=';
+        var ver2 = Message('hello, world').verify(addr2, signature1);
+        console.log(ver2);
+
+        var addr = 'n1ZCYg9YXtB5XCZazLxSmPDa8iwJRZHhGx';
+        var sig = 'IPynhAQDxJJV6mqWcowsCDXu7i7Xbc5D8M6Ai6UkTg8AfSQQvNS2RiLCIvDJtpVYG/mAV9pRTMjllKa8ZI43/64=';
+        var ver = Message('hello, world').verify(addr, sig);
+        console.log(ver);
+
+        var address = 'n1ZCYg9YXtB5XCZazLxSmPDa8iwJRZHhGx';
+        var signature = 'IFEG4TPPmd+N4GEG3raWMjuywXf7yXk5LWNnmofx8wKkR6Pyn1ZI5bCyqobFF0Xs5knEoNFZcK6Al91X0v8BS1A=';
+        var verified = Message('hello, world').verify(address, signature);
+        console.log(verified);
+
+        var address2 = 'n1ZCYg9YXtB5XCZazLxSmPDa8iwJRZHhGx';
+        var signature2 = 'H/DIn8uA1scAuKLlCx+/9LnAcJtwQQ0PmcPrJUq90aboLv3fH5fFvY+vmbfOSFEtGarznYli6ShPr9RXwY9UrIY=';
+        var verified2 = Message('hello, world').verify(address2, signature2);
+        console.log(verified2);
+
     }
 
     componentWillReceiveProps(nextProps) {
