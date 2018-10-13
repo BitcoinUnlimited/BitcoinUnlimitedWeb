aafdc46661df3baef629ea722e58368864f74e3b
Andrew Stone <g.andrew.stone@gmail.com>
2018-10-13 15:26:31 -0400

    fix sha256 for release

--- src/public/views/components/download/signatures_BUcash1-5-0-0.jsx
+++ src/public/views/components/download/signatures_BUcash1-5-0-0.jsx
@@ -3,17 +3,17 @@
 import React from 'react';
 import { strings } from '../../../lib/i18n';
 
-const CHECKSUMS =  `{ 'files': { 'BUcash-1.5.0.0-arm32.tar.gz': '2abfb1365206f10307b9e3edbc0c35f7e83b0f57c4ce0e6c3e1767c19014bb8b',
-             'BUcash-1.5.0.0-arm64.tar.gz': '726eba62d4c43a8746feb8f92816daade1cf7ad4d406f787183274a5013a7a06',
-             'BUcash-1.5.0.0-linux32.tar.gz': '8c9c4df8e4e510d8bb3f2d0015cd0fe7fb7d9eef58147644b3e215b40e820585',
-             'BUcash-1.5.0.0-linux64.tar.gz': '3ab98af2bb76e80f68a46d16d3db1fb236598dd725c1f464c45240edea4c736d',
-             'BUcash-1.5.0.0-osx.dmg': '3efaa7327aad0a570e0566112cfd00c6efe5194b162252dfa60bb7c8b278e132',
-             'BUcash-1.5.0.0-osx.tar.gz': '0a0be5d2fcd985a3e9dc66e52b6e71a3a0f34f8e663024dd0a410f60dd5baf1a',
-             'BUcash-1.5.0.0-osx64.tar.gz': '94ac8adc89036934458a447e9c196eadd56a2786ffbb8cdd2e666884aada1f05',
-             'BUcash-1.5.0.0-win32-setup.exe': '851be36bb9f0cb3d104646e7d130145a57f74f88583997274fc3410e80f2da21',
-             'BUcash-1.5.0.0-win32.zip': '11e95fad3ff1809f09a45c609aba6a8b17c160f41dffa54b14f73208c1ac82ec',
-             'BUcash-1.5.0.0-win64-setup.exe': '58847abe6b6805c737c0f0f79947080cf6afd686af2af7ca821b8deb8c4839ea',
-             'BUcash-1.5.0.0-win64.zip': '054a93750f1a7e3d0fe693bccb1d4fcdb824222831a0f603c32c1971d35ce47f'},
+const CHECKSUMS =  `{ 'files': { 'BUcash-1.5.0.0-arm32.tar.gz': '4123c618f7c68199a5afaace3e3aeb879e607f9dfe2fe5c4ad754bdfc814ffa2',
+             'BUcash-1.5.0.0-arm64.tar.gz': 'db9b2d6f1db65fa81234c35efe645950ae2428a7122fdb6b637db88f3efdf198',
+             'BUcash-1.5.0.0-linux32.tar.gz': 'de97abbfd108cefa72d25bdae86c9745f6f19102e9845b4ae06457cd2698c61b',
+             'BUcash-1.5.0.0-linux64.tar.gz': 'ffca9f54cc35fcf6cc5a8bd96b4b9c9efa3f474528ab29828d4e4b6e84e7e33d',
+             'BUcash-1.5.0.0-osx.dmg': 'e0a8bbe8d130bcebfbb68334c1f643a55e4eea9f5b225f8f7f84cfe915ad958a',
+             'BUcash-1.5.0.0-osx.tar.gz': 'ebaff06540d569f746b7c5c62b0e249cf89a3f6e05e5bac8fc5d7de0aeeb2285',
+             'BUcash-1.5.0.0-osx64.tar.gz': '0d5dd5357592083cfd2351e8bd98d71ed84d814791600980bd10463bc342ae9a',
+             'BUcash-1.5.0.0-win32-setup.exe': '2e820308fd0f49a23c1b1ccddef029992351a266ab35a44b30bf9ff5bd1dd751',
+             'BUcash-1.5.0.0-win32.zip': '5ad2c155021e6eddcf79c70cc932a32e75f35540bbaed63590c9249e95dda8af',
+             'BUcash-1.5.0.0-win64-setup.exe': '3ec5b54d6a9df6c57682751d5b29ff42c4cc0bb298b6e90f43c1c8728397f70d',
+             'BUcash-1.5.0.0-win64.zip': '5433f404700b332bcca442de2bf423499b51a5969248e506c1a05d5853202502'},
   'program': 'BitcoinUnlimitedCash',
   'version': '1.5.0.0'}`;
 
@@ -47,7 +47,7 @@ class SignaturesBitcoinCash extends React.Component {
                     {strings().download.signatures.andrew}<br/>
                     bitcoincash:qq9wwnuw4eukyh5g34ckg5vk4aaxnvr04vkspyv850<br/>
                     (legacy address format: 1zerg12nRXZ41Pw4tfCTqgtdiJx6D1We3)<br/>
-                    {strings().download.signatures.signature}:<br />HGrmd7MNsFqQjmXOXjwc84E5NrB/WWWI+0OhFnbf8zN8WVuU2ortV/zTIUPMdceEDpqySOmzwHDB7cJvA7H9PXo=
+                    {strings().download.signatures.signature}:<br />HJnMEhcJ4fb8D0JQ4n+cJdsfqxFUKLA5svu9JrkjIDLOIK0qfkSMTWImIS4PQqZ+h5HG84h2NaezohvMw6lszKQ=
                     <br/><br/>
                     {strings().download.signatures.sickpig}<br/>
                     bitcoincash:qrdvmdluf2s5cf08wcp9h2ja8lqt5peq35y56z4s7s<br/>
