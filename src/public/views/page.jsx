2048774d9bc16addae8dc2e3e018916e966fd108
Pat O'Brien <obrien317@gmail.com>
2018-11-23 15:05:35 -0600

    more responsive fixes

--- src/public/views/page.jsx
+++ src/public/views/page.jsx
@@ -7,20 +7,33 @@ import Section from './components/section.jsx';
 
 class Page extends React.Component {
 
+    displayTitle() {
+        let { title } = this.props;
+        return (title) ? (<div className="py2 h1 page-title">{ title }</div>) : null;
+    }
+
+    displaySubtitle() {
+        let { subtitle } = this.props;
+        return (subtitle) ? (<div className="py2 h3 page-subtitle">{ subtitle }</div>) : null;
+    }
+
+    displayIntro() {
+        let { intro } = this.props;
+        return (intro) ? (<div className='pb2 pt1 lh-copy page-intro'>{ intro }</div>) : null;
+    }
+
     render() {
         return (
             <div id={this.props.name}>
                 <Header active={this.props.name} />
-                <div className="p2 py4 center">
+                <div className="p2 py4 center page-wrapper">
                     <div className={`section__container container-${this.props.name}`}>
-                        <div className="py2 h1">{ this.props.title }</div>
-                        <div className="py2 h3">{ this.props.subtitle }</div>
-                        { this.props.intro ? <div className='pb2 pt1 lh-copy'>{this.props.intro}</div> : '' }
-
+                        { this.displayTitle() }
+                        { this.displaySubtitle() }
+                        { this.displayIntro() }
                         <div className='left-align p2 pb4'>
                             { this.props.children }
                         </div>
-
                     </div>
                 </div>
                 <Footer />
