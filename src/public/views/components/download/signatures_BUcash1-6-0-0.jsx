8b63dfe6e72fb837084dd4d6771806cf825896af
Andrea Suisani <sickpig@gmail.com>
2019-04-24 14:45:03 +0000

    Add Greg's signature for the 1.6.0 release

--- src/public/views/components/download/signatures_BUcash1-6-0-0.jsx
+++ src/public/views/components/download/signatures_BUcash1-6-0-0.jsx
@@ -57,7 +57,7 @@ class SignaturesBitcoinCash extends React.Component {
                     {strings().download.signatures.griffith}<br/>
                     bitcoincash:qrupj2nuskyg4dmdrpnytjs4p8s6av542cuja2qdzy<br/>
                     (legacy address format: 1Pcpj8sR5adRNMi8rZ1Zi7rg6GPqcmFHAz)<br/>
-                    {strings().download.signatures.signature}:<br />TBD
+                    {strings().download.signatures.signature}:<br />IBxujC7ZYX+W/pyWC7IfoE0FLA9I5lko7F3uMezMfuovABo6F41zyAKziWoVHg2htGhlWaSwGgDxK75SJbeikbw=
                     <br/>
                     <br/>
                 </div>
