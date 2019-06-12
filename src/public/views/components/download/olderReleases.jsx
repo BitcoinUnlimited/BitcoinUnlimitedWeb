d0403282288b0d3c627a1ac62e2eb5d9c6fcbeee
Søren Bredlund Caspersen <soeren.b.c@gmail.com>
2019-06-12 11:38:14 +0200

    BUIP 115 (#217)
    Move official BTC release to 'Older Releases'

--- src/public/views/components/download/olderReleases.jsx
+++ src/public/views/components/download/olderReleases.jsx
@@ -14,6 +14,7 @@ import Signatures0112 from './signatures_0-11-2.jsx'
 import Signatures1013 from './signatures_1-0-1-3.jsx'
 import Signatures1014 from './signatures_1-0-1-4.jsx'
 import Signatures1020 from './signatures_1-0-2-0.jsx'
+import Signatures1030 from './signatures_1-0-3-0.jsx'
 import SignaturesBUcash1100 from './signatures_BUcash1-1-0-0.jsx'
 import SignaturesBUcash1110 from './signatures_BUcash1-1-1-0.jsx'
 import SignaturesBUcash1111 from './signatures_BUcash1-1-1-1.jsx'
@@ -169,6 +170,25 @@ class OfficialRelease extends React.Component {
             </Release>
         )
     }
+    renderV1030Body() {
+        return (
+                <Release
+                    osx64='https://github.com/BitcoinUnlimited/BitcoinUnlimitedWebDownloadHistory/raw/master/bitcoinUnlimited-1.0.3.0-osx64.tar.gz'
+                    osx32='https://github.com/BitcoinUnlimited/BitcoinUnlimitedWebDownloadHistory/raw/master/bitcoinUnlimited-1.0.3.0-osx.tar.gz'
+                    osxDmg='https://github.com/BitcoinUnlimited/BitcoinUnlimitedWebDownloadHistory/raw/master/bitcoinUnlimited-1.0.3.0-osx.dmg'
+                    windows64Exe='https://github.com/BitcoinUnlimited/BitcoinUnlimitedWebDownloadHistory/raw/master/bitcoinUnlimited-1.0.3.0-win64-setup.exe'
+                    windows32Exe='https://github.com/BitcoinUnlimited/BitcoinUnlimitedWebDownloadHistory/raw/master/bitcoinUnlimited-1.0.3.0-win32-setup.exe'
+                    linux64='https://github.com/BitcoinUnlimited/BitcoinUnlimitedWebDownloadHistory/raw/master/bitcoinUnlimited-1.0.3.0-linux64.tar.gz'
+                    linux32='https://github.com/BitcoinUnlimited/BitcoinUnlimitedWebDownloadHistory/raw/master/bitcoinUnlimited-1.0.3.0-linux32.tar.gz'
+                    arm64='https://github.com/BitcoinUnlimited/BitcoinUnlimitedWebDownloadHistory/raw/master/bitcoinUnlimited-1.0.3.0-arm64.tar.gz'
+                    arm32='https://github.com/BitcoinUnlimited/BitcoinUnlimitedWebDownloadHistory/raw/master/bitcoinUnlimited-1.0.3.0-arm32.tar.gz'
+                    source='https://github.com/BitcoinUnlimited/BitcoinUnlimited/releases/tag/v1.0.3.0'>
+
+                    <Signatures1030 />
+                </Release>
+        )
+    }
+
     renderV1020Body() {
         return (
             <Release
@@ -379,6 +399,11 @@ class OfficialRelease extends React.Component {
                 <Section
                     title='BUcash-1.1.0.0'
                     body={ this.renderVBUcash1100Body() }
+                    expanded={ false } />
+
+                <Section
+                    title='v1.0.3.0'
+                    body={ this.renderV1030Body() }
                     expanded={ false } />
 
                 <Section
