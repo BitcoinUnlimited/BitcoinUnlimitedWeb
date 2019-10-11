98a73544b098534b5f0b22b2f3fe6982c646d7fc
Andrea Suisani <sickpig@gmail.com>
2019-10-11 18:13:26 +0000

    release 1.7.0.0 with electrscash binary

--- src/public/views/components/download/signatures_BUcash1-7-0-0.jsx
+++ src/public/views/components/download/signatures_BUcash1-7-0-0.jsx
@@ -3,17 +3,17 @@
 import React from 'react';
 import { strings } from '../../../lib/i18n';
 
-const CHECKSUMS =  `{ 'files': { 'BUcash-1.7.0.0-arm32.tar.gz': 'f9c277ca84541a9d2f8c3164298dd5aafbb1f3aa237133b47ec2f58108bda615',
-             'BUcash-1.7.0.0-arm64.tar.gz': '28f3eb0b9940ecf9de8f62e44cf648c83e322b3b49f9dc95206aa3a6b567532a',
-             'BUcash-1.7.0.0-linux32.tar.gz': 'a8d3dcd8304291b058efbf3ebeca834bd9968e767f8b4245d2074e71b9a1905d',
-             'BUcash-1.7.0.0-linux64.tar.gz': '4122a3e25e963e4f63f82c931fbdad42500611533f93c8d89ed3570d6f54eb05',
-             'BUcash-1.7.0.0-osx.dmg': '7ebb3d4743a6a68cc489601d5760c867a6c78dc79c688ce61137cede3497cae2',
-             'BUcash-1.7.0.0-osx.tar.gz': '060422d95c86231c1cb67bb8bb767b864a1fee76a4e1c18cc0eb67a0d8020e97',
-             'BUcash-1.7.0.0-osx64.tar.gz': '5b33975d666fcbd986621eff0bcd75194180252aaab46a62af394aaa2d8302b6',
-             'BUcash-1.7.0.0-win32-setup.exe': 'f94ed7fc1252bc15cf538161a03d2a73be45529892e4aeff59e80888f3519a29',
-             'BUcash-1.7.0.0-win32.zip': '1ce86d6085738622d362623292ff96cfeb0a34c7d212491ecb66f3f3c40e68cc',
-             'BUcash-1.7.0.0-win64-setup.exe': '451be2d3968c5821f89841239b692fe3dac0605217135f0803cee82b40983e0f',
-             'BUcash-1.7.0.0-win64.zip': '24344b07faef61f9482fe6b96ccb0c295a6cd2677b94f9bca357ad7a281ac5f6'},
+const CHECKSUMS =  `{ 'files': { 'BUcash-1.7.0.0-arm32.tar.gz': '1fecb8cf47878a26abf8201ca5cce78e266d8f0097c748c383b5b7fafa665798',
+             'BUcash-1.7.0.0-arm64.tar.gz': 'c17cebb1d04d381aba6fa76233a11017cd1a5932d8cb0aefd6a430174045e738',
+             'BUcash-1.7.0.0-linux32.tar.gz': '82f4eeae25c8e1b95fc9b3c98af802388f0b28365bace54af0c88ace67a90d55',
+             'BUcash-1.7.0.0-linux64.tar.gz': '66345fd4b813f4c5a704ea7fdd60027be467008b10cf32db203146724128d140',
+             'BUcash-1.7.0.0-osx.dmg': 'd0e231c6d5305609063c9f978dda62c92e0d6c26dbe0754e364d1ccb2c4d9bbc',
+             'BUcash-1.7.0.0-osx.tar.gz': 'a09ff385f2488e66fda38e69c401cfcdfc8db053221f9155855b4b3b9b8edc0b',
+             'BUcash-1.7.0.0-osx64.tar.gz': 'd0e3d2d5386e9501819095d0060f48be940c0fc9f84e6bc97eb03674d8986514',
+             'BUcash-1.7.0.0-win32-setup.exe': 'f0bc6a5797e2c5fa585af08022e19a52a51945af4e17346089711c0d1304e080',
+             'BUcash-1.7.0.0-win32.zip': 'c4609a574ec8978b7512f6551929c55ae4cc2894c5055dfacd08c84023e1200c',
+             'BUcash-1.7.0.0-win64-setup.exe': '5664f6c1d13fc4a0194ed66e05072e2b6a46e86f14b84cf23a5497f5732aad53',
+             'BUcash-1.7.0.0-win64.zip': 'acb37a4cef821de500d7890dddac57c1b63ec370ba40cd4159ea58609682930b'},
   'program': 'Bitcoin Unlimited Cash',
   'version': '1.7.0.0'}`;
 
@@ -47,17 +47,17 @@ class SignaturesBitcoinCash extends React.Component {
                     {strings().download.signatures.andrew}<br/>
                     bitcoincash:qq9wwnuw4eukyh5g34ckg5vk4aaxnvr04vkspyv850<br/>
                     (legacy address format: 1zerg12nRXZ41Pw4tfCTqgtdiJx6D1We3)<br/>
-                    {strings().download.signatures.signature}: <br/> HKFjN/PO5xqG8qma/kVnOkOOO9BotqWRU8xkmWoBDUiXBedfxzsn/k5L4l5MYi/v8srym6BjiAPr0b99wF6+zEg=
+                    {strings().download.signatures.signature}: <br/> G0S5rSzbFq2/M+unbdYaKOvaW6UPvXDXjnVWP2kmIa0oYdzvGBE3wVdk5XINxyk0kHsQMEl6NBVQ+r7jS6HdXdU=
                     <br/><br/>
                     {strings().download.signatures.sickpig}<br/>
                     bitcoincash:qrdvmdluf2s5cf08wcp9h2ja8lqt5peq35y56z4s7s<br/>
                     (legacy address format: 1LwvkQTWmotqTosgBcK8kFPCKzW2BPiE1G)<br/>
-                    {strings().download.signatures.signature}: <br/> HzBDCJo97RIZvzMA0LjYl/bXA6LB/igNmFUozaoAA0Xaf06gOwoPy/WVEokuXqNgHfoM5iSviWwq8byz07Thun8=
+                    {strings().download.signatures.signature}: <br/> H8eX3ALL1/FC2BfMQIx4sNpZtpihyTXmqJ5/4fjcVtveDx0Iqd97W0CqsT2VdLGmhb4lEHf6rlDWzovPDuYsh3w=
                     <br/><br/>
                     {strings().download.signatures.griffith}<br/>
                     bitcoincash:qrupj2nuskyg4dmdrpnytjs4p8s6av542cuja2qdzy<br/>
                     (legacy address format: 1Pcpj8sR5adRNMi8rZ1Zi7rg6GPqcmFHAz)<br/>
-                    {strings().download.signatures.signature}: <br/> Hw0u8xe199/ea+LohmvighkHQTnL/r38geU0UyOoBnH3c2Ql4sKirqoG52UsNoPRDXFgvQ3wbTKP//TPtF6TM2g=
+                    {strings().download.signatures.signature}: <br/> TBD
                     <br/>
                     <br/>
                 </div>
