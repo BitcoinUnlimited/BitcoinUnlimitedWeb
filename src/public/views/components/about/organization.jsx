a859cf21fcbf40000569f84f648c323f17c7d44b
Andrea Suisani <sickpig@gmail.com>
2021-11-16 14:27:12 +0000

    Add a reference to the unincorporated version of the AoF

--- src/public/views/components/about/organization.jsx
+++ src/public/views/components/about/organization.jsx
@@ -9,7 +9,7 @@ class Organization extends React.Component {
         return (
             <div>
                 <p className='lh-copy'>
-                  The Bitcoin Unlimited organization maintains the <a className='link--underline dim black' href="https://github.com/BitcoinUnlimited/BitcoinUnlimited" target="_blank">open-source Bitcoin client</a> by the same name. The organization's mandates are:
+                  The Bitcoin Unlimited organization maintains the <a className='link--underline dim black' href="https://gitlab.com/bitcoinunlimited/BCHUnlimited" target="_blank">open-source Bitcoin client</a> by the same name. The organization's mandates are:
                 </p>
 
                 <ol>
@@ -18,10 +18,10 @@ class Organization extends React.Component {
                 </ol>
 
                 <p className='lh-copy'>
-                  The organization is governed by its <a className='link--underline dim black' href="/resources/BUarticles_new.pdf" target="_blank">Articles of Federation [PDF]</a> and controversial decisions are settled by members voting. The organization is funded by donations, and it <a className='link--underline dim black' href="https://medium.com/@peter_r/call-for-proposals-making-satoshis-vision-a-reality-with-bitcoin-unlimited-4ff5da246dc7" target="_blank">awards grants</a> to qualified groups and individuals to help fund projects in line with its mandates.
+                  B.U. Inc. is governed by its <a className='link--underline dim black' href="/resources/BUarticles_new.pdf" target="_blank">Articles of Federation [PDF]</a> and controversial decisions are settled by members voting. The organization is funded by donations, and it <a className='link--underline dim black' href="https://medium.com/@peter_r/call-for-proposals-making-satoshis-vision-a-reality-with-bitcoin-unlimited-4ff5da246dc7" target="_blank">awards grants</a> to qualified groups and individuals to help fund projects in line with its mandates.
                 </p>
                 <p className='lh-copy'>
-                  Bitcoin Unlimited does not allow overhead when giving to universities or other institutions.
+		  Bitcoin Unlimited does not allow overhead when giving to universities or other institutions. The unincorporated <a className='link--underline dim black' href="/resources/BUarticles_old.pdf" target="_blank">Articles of Federation (PDF)</a> are here for reference only.
                 </p>
             </div>
 
