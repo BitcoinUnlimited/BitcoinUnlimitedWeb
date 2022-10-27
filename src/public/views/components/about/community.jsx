e5ce356c6c2a74bc827525295c96cef1c8feac60
Andrea Suisani <sickpig@gmail.com>
2022-10-27 12:21:58 +0000

    Update BU forum URL

--- src/public/views/components/about/community.jsx
+++ src/public/views/components/about/community.jsx
@@ -10,27 +10,26 @@ class Community extends React.Component {
             <div>
             <ul className='py1'>
                 <li className='pb1'>
-                    <img className='icon--center pr1' src="../../img/twitter.png" alt="Twitter" />&nbsp;
-                    <a className="link--underline dim black" href="https://twitter.com/bitcoinunlimit">
-                        { strings().about.community.twitter1 }&nbsp;
-                        { strings().about.community.twitter2 }&nbsp;
-                        { strings().about.community.twitter3 }
-                    </a>
+                    <p>
+                        <img className='icon--center pr1' src="../../img/twitter.png" alt="Twitter" />&nbsp;
+                        <a className="link--underline dim black" href="https://twitter.com/bitcoinunlimit">
+                            { strings().about.community.twitter1 }&nbsp;
+                            { strings().about.community.twitter2 }&nbsp;
+                            { strings().about.community.twitter3 }
+                        </a>
+                    </p>
                 </li>
                 <li className='pb1'>
                     <p>
-
                         { strings().about.community.buip1 }&nbsp;
                         <b>
                             { strings().about.community.buip2 }
                         </b>&nbsp;
                         { strings().about.community.buip3 }&nbsp;
                         <a className="link--underline dim black"
-                            href="https://bitco.in/forum/forums/bitcoin-unlimited.15/">
+                            href="https://forum.bitcoinunlimited.info/">
                             { strings().about.community.buip4 }
-                        </a>&nbsp;
-                        { strings().about.community.buip5 }
-
+                        </a>
                     </p>
                 </li>
                 <li className='pb1'>
